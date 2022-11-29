014f847ae3f690d9179bb9eb682071edda260feb
shinung-han <hanshinung@gmail.com>
2022-11-29 15:24:30 +0900

    add files

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  black: '#000000',
+  white: '#FFFFFF',
+  lightGrey: '#B0B0B0',
+  middleGrey: '#717171',
+  deepGrey: '#222222',
+  hoverGrey: '#DBDBDB',
+  blue: '#2E86C1',
+  red: '#F1341A',
+};
